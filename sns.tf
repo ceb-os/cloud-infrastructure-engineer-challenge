49fe167707cbcf50193aa2ea5066295948c21782
ceb-os <sucari.sebastian@gmail.com>
2025-10-16 11:00:49 -0300

    created sns topic

---
+++ sns.tf
@@ -0,0 +1,9 @@
+resource "aws_sns_topic" "rds_alarms_topic" {
+    name = "nanlabs-sns-rds-alarms"
+}
+
+resource "aws_sns_topic_subscription" "email_subscription" {
+    topic_arn = aws_sns_topic.rds_alarms_topic.arn
+    protocol = "email"
+    endpoint = "sebasucari@gmail.com"
+}
